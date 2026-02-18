b06ea87f5166a58ec8678ec0b6c1a38bfbaaaeb2
eyehypnotizer <lavanyasrirenu14112005@gmail.com>
2026-02-18 10:30:53 +0530

    Time: 4 ms (99.69%), Space: 45.9 MB (88%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int num=x;
+        int rev=0;
+        
+        while(x!=0){
+            int lastdigit=x%10;
+            rev=rev*10+lastdigit;
+            x=x/10;
+        }
+        return num==rev;
+    }
+}
