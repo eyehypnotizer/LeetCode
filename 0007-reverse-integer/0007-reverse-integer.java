6f16137b84ac29cc281268fc872e71e952bd7f36
eyehypnotizer <lavanyasrirenu14112005@gmail.com>
2026-02-18 10:13:36 +0530

    Time: 1 ms (99.8%), Space: 42.9 MB (26.37%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+      int rev=0;
+      while(x!=0){
+        int lastdigit=x%10;
+        if(rev > Integer.MAX_VALUE / 10 || rev == Integer.MAX_VALUE / 10 && lastdigit > 7){
+            return 0;
+        }
+
+        if(rev < Integer.MIN_VALUE / 10 || rev == Integer.MIN_VALUE / 10 && lastdigit < -8){
+            return 0;
+        }
+
+        rev=rev*10+lastdigit;
+        x=x/10;
+      }  
+      return rev;
+    }
+}
